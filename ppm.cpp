659c461fa4c598d1173c20f7ac6c6dd383d92fda
sticky <keai21@student.bth.se>
2024-10-09 19:49:00 +0200

    t

---
+++ ppm.cpp
@@ -0,0 +1,5 @@
+//
+// Created by pc1admin on 2024-10-09.
+//
+
+#include "ppm.h"
---
+++ ppm.h
@@ -0,0 +1,8 @@
+//
+// Created by pc1admin on 2024-10-09.
+//
+
+#ifndef UNTITLED4_PPM_H
+#define UNTITLED4_PPM_H
+
+#endif //UNTITLED4_PPM_H

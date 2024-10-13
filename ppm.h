94daecc0bb0d2fbef23ea76c6bebe2273243cdaa
kevin wigren <89857252+StickyTheStick12@users.noreply.github.com>
2024-10-13 09:21:19 +0200

    Update ppm.h

--- ppm.h
+++ ppm.h
@@ -1,6 +1,4 @@
-#include "matrix.hpp"
-#include <exception>
-#include <iostream>
+#include "matrix.h"
 #include <sstream>
 
 #if !defined(PPM_READER_HPP)
@@ -14,17 +12,8 @@ constexpr unsigned max_pixels { max_dimension * max_dimension };
 constexpr char const* magic_number { "P6" };
 
 class Reader {
-private:
-    std::stringstream stream;
-
-    std::string get_magic_number();
-    std::pair<unsigned, unsigned> get_dimensions();
-    std::tuple<unsigned char*, unsigned char*, unsigned char*> get_data(unsigned x_size, unsigned y_size);
-    unsigned get_color_max();
-    void fill(std::string filename);
-
 public:
-    Matrix operator()(std::string filename);
+    Matrix operator()(const std::string& filename);
 };
 
 class Writer {
